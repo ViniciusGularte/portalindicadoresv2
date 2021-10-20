c2e8956c8cb55567e328ef20e3c576c252fa6a2b
vinicius <viniciusgulartemartin@gmail.com>
2021-10-20 20:37:13 -0300

    Updates

---
+++ precache-manifest.dd537f19a972323fcc4ba917cf687416.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c594a49277b6fa5a0b2095a9e2c6d4",
+    "url": "/portalindicadores/index.html"
+  },
+  {
+    "revision": "ed33e3fd81d6ec0e216e",
+    "url": "/portalindicadores/static/css/4.ad459609.chunk.css"
+  },
+  {
+    "revision": "f6245709789e2f87be22",
+    "url": "/portalindicadores/static/css/main.b80e3ecc.chunk.css"
+  },
+  {
+    "revision": "2cab482267e55b4ca4ba",
+    "url": "/portalindicadores/static/js/0.6a3c337f.chunk.js"
+  },
+  {
+    "revision": "22a34a2e01e4c0e02b10",
+    "url": "/portalindicadores/static/js/10.f34a3674.chunk.js"
+  },
+  {
+    "revision": "c04567a92d527e7d2a39",
+    "url": "/portalindicadores/static/js/11.0a7317a5.chunk.js"
+  },
+  {
+    "revision": "106f7182fcbe290753a4",
+    "url": "/portalindicadores/static/js/12.f515bc61.chunk.js"
+  },
+  {
+    "revision": "191a72cfcdbf2b0d3e09",
+    "url": "/portalindicadores/static/js/13.e55e31a7.chunk.js"
+  },
+  {
+    "revision": "0dcfc25043dfce96ed19",
+    "url": "/portalindicadores/static/js/14.59ccd350.chunk.js"
+  },
+  {
+    "revision": "e249202def680bc1a6fe",
+    "url": "/portalindicadores/static/js/15.39e2263c.chunk.js"
+  },
+  {
+    "revision": "c18483689edc81054241",
+    "url": "/portalindicadores/static/js/16.b3f30771.chunk.js"
+  },
+  {
+    "revision": "1ddfc7a02e44314e1e8f",
+    "url": "/portalindicadores/static/js/17.8dacd7fe.chunk.js"
+  },
+  {
+    "revision": "e1fa6cb4de5812662ca8",
+    "url": "/portalindicadores/static/js/18.cc78d009.chunk.js"
+  },
+  {
+    "revision": "f1edee438622c474736c",
+    "url": "/portalindicadores/static/js/19.373011d0.chunk.js"
+  },
+  {
+    "revision": "37fff6c2bc0abfc3f4ba",
+    "url": "/portalindicadores/static/js/20.de8ae754.chunk.js"
+  },
+  {
+    "revision": "7c2218046b392d70a157",
+    "url": "/portalindicadores/static/js/21.3fff82e1.chunk.js"
+  },
+  {
+    "revision": "58e8aa78a513a320997e",
+    "url": "/portalindicadores/static/js/22.cd880a52.chunk.js"
+  },
+  {
+    "revision": "687fc84d0e7312b9826b",
+    "url": "/portalindicadores/static/js/23.ad3d95fa.chunk.js"
+  },
+  {
+    "revision": "ddc9e442764986249b79",
+    "url": "/portalindicadores/static/js/24.e0214239.chunk.js"
+  },
+  {
+    "revision": "df0e4884e98c064bcb86",
+    "url": "/portalindicadores/static/js/25.64582417.chunk.js"
+  },
+  {
+    "revision": "ff87adce9470f760c7ec",
+    "url": "/portalindicadores/static/js/26.ce96893a.chunk.js"
+  },
+  {
+    "revision": "56a53698fa0f39591fd1",
+    "url": "/portalindicadores/static/js/27.0568eed0.chunk.js"
+  },
+  {
+    "revision": "4fe9c40a38bbff59ff1f",
+    "url": "/portalindicadores/static/js/28.5fe2b6f5.chunk.js"
+  },
+  {
+    "revision": "d8b872744365ac50af5f",
+    "url": "/portalindicadores/static/js/29.cfee5d48.chunk.js"
+  },
+  {
+    "revision": "ae1156e04c9cb09525c2",
+    "url": "/portalindicadores/static/js/30.e914ef5f.chunk.js"
+  },
+  {
+    "revision": "13732e070a0ccde0b6fc",
+    "url": "/portalindicadores/static/js/31.5f2f69fa.chunk.js"
+  },
+  {
+    "revision": "ad38c6a2b5f6c7c2d161",
+    "url": "/portalindicadores/static/js/32.0eb3d23b.chunk.js"
+  },
+  {
+    "revision": "e8e408f912c314851c5c",
+    "url": "/portalindicadores/static/js/33.cc2f2bfd.chunk.js"
+  },
+  {
+    "revision": "52f9d963e5c345ffea35",
+    "url": "/portalindicadores/static/js/34.6947e55d.chunk.js"
+  },
+  {
+    "revision": "74ece90a9bfce5f5cd78",
+    "url": "/portalindicadores/static/js/35.be60385d.chunk.js"
+  },
+  {
+    "revision": "7412135b6261a0214e14",
+    "url": "/portalindicadores/static/js/36.7e6a97e2.chunk.js"
+  },
+  {
+    "revision": "d7413e115ad5617e2b58",
+    "url": "/portalindicadores/static/js/37.f690d328.chunk.js"
+  },
+  {
+    "revision": "1d50331ef92227a75ca3",
+    "url": "/portalindicadores/static/js/38.ce57b943.chunk.js"
+  },
+  {
+    "revision": "ab18544f472449a33983",
+    "url": "/portalindicadores/static/js/39.a1859708.chunk.js"
+  },
+  {
+    "revision": "ed33e3fd81d6ec0e216e",
+    "url": "/portalindicadores/static/js/4.fe61acb8.chunk.js"
+  },
+  {
+    "revision": "f0d45ed24b07eeac8402",
+    "url": "/portalindicadores/static/js/40.8b5ab326.chunk.js"
+  },
+  {
+    "revision": "0edca512be080eee2b22",
+    "url": "/portalindicadores/static/js/41.833a3e8a.chunk.js"
+  },
+  {
+    "revision": "afed4cf70aa3d2e1ec79",
+    "url": "/portalindicadores/static/js/42.63e3477d.chunk.js"
+  },
+  {
+    "revision": "5e21ec78bd832daf1356",
+    "url": "/portalindicadores/static/js/43.e607d1b4.chunk.js"
+  },
+  {
+    "revision": "3689d1105e5e1cb601da",
+    "url": "/portalindicadores/static/js/44.2ae6957f.chunk.js"
+  },
+  {
+    "revision": "5894ddfd2d15a46d9613",
+    "url": "/portalindicadores/static/js/45.a6a835c0.chunk.js"
+  },
+  {
+    "revision": "0f2093c8ab72850a829a",
+    "url": "/portalindicadores/static/js/46.ade3bd09.chunk.js"
+  },
+  {
+    "revision": "c25df9c1fc46efadb91d",
+    "url": "/portalindicadores/static/js/47.bba9829c.chunk.js"
+  },
+  {
+    "revision": "7eab5f6868b9063abf7e",
+    "url": "/portalindicadores/static/js/48.c4475342.chunk.js"
+  },
+  {
+    "revision": "b019db07297f47ef5494",
+    "url": "/portalindicadores/static/js/49.c2e5715f.chunk.js"
+  },
+  {
+    "revision": "6bba1b2890a869ed3e6b",
+    "url": "/portalindicadores/static/js/5.173dd3b8.chunk.js"
+  },
+  {
+    "revision": "c40acd95ac4ac3c8870d",
+    "url": "/portalindicadores/static/js/50.bf1ad231.chunk.js"
+  },
+  {
+    "revision": "b8e5aabffb297f133534",
+    "url": "/portalindicadores/static/js/51.cbecc9e3.chunk.js"
+  },
+  {
+    "revision": "2cc6931550adaeefcce0",
+    "url": "/portalindicadores/static/js/52.d6734ef9.chunk.js"
+  },
+  {
+    "revision": "062af94f2010b09b5536",
+    "url": "/portalindicadores/static/js/53.7b29b5d5.chunk.js"
+  },
+  {
+    "revision": "98d60212f848a7288fb2",
+    "url": "/portalindicadores/static/js/54.81c50d4b.chunk.js"
+  },
+  {
+    "revision": "00302858d490e626d4c1",
+    "url": "/portalindicadores/static/js/55.e18f417f.chunk.js"
+  },
+  {
+    "revision": "98b923ea5f78a5f67fce",
+    "url": "/portalindicadores/static/js/56.7d362d9a.chunk.js"
+  },
+  {
+    "revision": "c1f2cf66a6ea3d6370e9",
+    "url": "/portalindicadores/static/js/57.62a85e45.chunk.js"
+  },
+  {
+    "revision": "908120b13496ae349ee5",
+    "url": "/portalindicadores/static/js/58.8a84ea2f.chunk.js"
+  },
+  {
+    "revision": "ec047b1a4e678ce292e3",
+    "url": "/portalindicadores/static/js/59.0d8d8cc7.chunk.js"
+  },
+  {
+    "revision": "c3b21a9458491636f7b7",
+    "url": "/portalindicadores/static/js/6.50b9380d.chunk.js"
+  },
+  {
+    "revision": "a0c3974e59644861023d",
+    "url": "/portalindicadores/static/js/60.4b9bdbcd.chunk.js"
+  },
+  {
+    "revision": "88fa0c9eb7574035bbc3",
+    "url": "/portalindicadores/static/js/7.fe710889.chunk.js"
+  },
+  {
+    "revision": "11e5106b852638f271de",
+    "url": "/portalindicadores/static/js/8.b31530b4.chunk.js"
+  },
+  {
+    "revision": "78f38f94e9b148878865",
+    "url": "/portalindicadores/static/js/9.83ed1887.chunk.js"
+  },
+  {
+    "revision": "f6245709789e2f87be22",
+    "url": "/portalindicadores/static/js/main.d1669402.chunk.js"
+  },
+  {
+    "revision": "d12f01bde7c4989d81ba",
+    "url": "/portalindicadores/static/js/polyfills-dom.8e5da81a.chunk.js"
+  },
+  {
+    "revision": "532980652a977860191f",
+    "url": "/portalindicadores/static/js/runtime-main.39700fc8.js"
+  }
+]);
